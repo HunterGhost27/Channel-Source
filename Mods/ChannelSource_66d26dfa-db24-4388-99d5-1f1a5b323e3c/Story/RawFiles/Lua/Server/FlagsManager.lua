5d29291cf860efe845a8844e4a0cf22a94b3fe5f
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-02-09 22:39:30 +0530

    Add Toggle Console-Commands

--- Mods/ChannelSource_66d26dfa-db24-4388-99d5-1f1a5b323e3c/Story/RawFiles/Lua/Server/FlagsManager.lua
+++ Mods/ChannelSource_66d26dfa-db24-4388-99d5-1f1a5b323e3c/Story/RawFiles/Lua/Server/FlagsManager.lua
@@ -6,3 +6,31 @@ Flags:Track({['flagName'] = 'S7_ExtenderAvailable'})
 Flags:Track({['flagName'] = 'S7_ChannelSource_GiveMeBooks'})
 Flags:Track({['flagName'] = 'S7_ChannelSource_DUBA'})
 Flags:Track({['flagName'] = 'S7_CS_MutualCooldown'})
+
+--  =======================
+--  CONSOLE COMMAND TOGGLES
+--  =======================
+
+--  DUBA
+--  ====
+
+ConsoleCommander:Register({
+    ['Name'] = 'ToggleCompatibilityMode',
+    ['Description'] = 'Toggles Script-Based Compatibility Mode',
+    ['Context'] = 'Server',
+    ['Action'] = function ()
+        Flags['S7_ChannelSource_DUBA']:Toggle()
+    end
+})
+
+--  MUTUAL COOLDOWNS
+--  ================
+
+ConsoleCommander:Register({
+    ['Name'] = 'ToggleMutualCooldown',
+    ['Description'] = 'Toggles Channel-Source Mutual-Cooldowns',
+    ['Context'] = 'Server',
+    ['Action'] = function ()
+        Flags['S7_CS_MutualCooldown']:Toggle()
+    end
+})
