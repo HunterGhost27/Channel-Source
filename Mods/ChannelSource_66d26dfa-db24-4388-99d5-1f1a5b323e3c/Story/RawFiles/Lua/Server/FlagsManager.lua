7e309410d3a0abb5688e0f1b00b4ab7e0d815df1
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-02-09 20:41:31 +0530

    Consolidated DUBA Flags

--- Mods/ChannelSource_66d26dfa-db24-4388-99d5-1f1a5b323e3c/Story/RawFiles/Lua/Server/FlagsManager.lua
+++ Mods/ChannelSource_66d26dfa-db24-4388-99d5-1f1a5b323e3c/Story/RawFiles/Lua/Server/FlagsManager.lua
@@ -4,6 +4,5 @@
 
 Flags:Track({['flagName'] = 'S7_ExtenderAvailable'})
 Flags:Track({['flagName'] = 'S7_ChannelSource_GiveMeBooks'})
-Flags:Track({['flagName'] = 'S7_ChannelSource_DUBA_Enabled'})
-Flags:Track({['flagName'] = 'S7_ChannelSource_DUBA_Disabled'})
+Flags:Track({['flagName'] = 'S7_ChannelSource_DUBA'})
 Flags:Track({['flagName'] = 'S7_CS_MutualCooldown'})
