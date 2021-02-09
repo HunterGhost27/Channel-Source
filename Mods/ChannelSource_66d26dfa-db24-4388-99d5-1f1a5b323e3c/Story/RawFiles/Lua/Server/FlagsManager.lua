11d6b3c88e8fc3237179c46fcd84bdee445cb32e
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-02-09 20:32:15 +0530

    Fix FlagsManager

--- Mods/ChannelSource_66d26dfa-db24-4388-99d5-1f1a5b323e3c/Story/RawFiles/Lua/Server/FlagsManager.lua
+++ Mods/ChannelSource_66d26dfa-db24-4388-99d5-1f1a5b323e3c/Story/RawFiles/Lua/Server/FlagsManager.lua
@@ -2,10 +2,8 @@
 -- FLAGS MANAGER
 -- =============
 
-Flags:Track({['Name'] = 'S7_ExtenderAvailable'})
-Flags:Track({['Name'] = 'S7_CS_ModAddedTo_NewGame'})
-Flags:Track({['Name'] = 'S7_CS_ModAddedTo_ExistingSave'})
-Flags:Track({['Name'] = 'S7_ChannelSource_GiveMeBooks'})
-Flags:Track({['Name'] = 'S7_ChannelSource_DUBA_Enabled'})
-Flags:Track({['Name'] = 'S7_ChannelSource_DUBA_Disabled'})
-Flags:Track({['Name'] = 'S7_CS_MutualCooldown'})
+Flags:Track({['flagName'] = 'S7_ExtenderAvailable'})
+Flags:Track({['flagName'] = 'S7_ChannelSource_GiveMeBooks'})
+Flags:Track({['flagName'] = 'S7_ChannelSource_DUBA_Enabled'})
+Flags:Track({['flagName'] = 'S7_ChannelSource_DUBA_Disabled'})
+Flags:Track({['flagName'] = 'S7_CS_MutualCooldown'})
